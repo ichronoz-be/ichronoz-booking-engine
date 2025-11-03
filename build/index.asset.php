f5af3240ad3e64c4a05388b002be2cda82dea242
ichronoz-be <rifa@ichronoz.com>
2025-11-03 12:58:21 +0700

    fix btn-link

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'd312dcdf71654493e253');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '58d9ff7ab1dd5d381e60');
