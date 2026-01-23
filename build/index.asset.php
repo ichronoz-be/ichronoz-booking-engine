9936ab8ad5753e38126439b13d0f38836805c30e
ichronoz-be <rifa@ichronoz.com>
2026-01-24 05:53:07 +0700

    fix qty more then 1

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '0c1697257d7f2824abfe');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '9537f1e2079622fe135a');
